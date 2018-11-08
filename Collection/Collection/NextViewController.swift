6ef64bf5725ad4560ab21ee8d4d4cb8afd0bdfbc
kagari <e175766@ie.u-ryukyu.ac.jp>
2018-11-08 23:48:01 +0900

    検索画面などのUIを追加した

---
+++ Collection/Collection/NextViewController.swift
@@ -0,0 +1,36 @@
+//
+//  NextViewController.swift
+//  Collection
+//
+//  Created by 大城昂希 on 2018/11/08.
+//  Copyright © 2018 大城昂希. All rights reserved.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    @IBOutlet weak var iconLabel: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var textLabel: UITextView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Collection/Collection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Collection
+//
+//  Created by 大城昂希 on 2018/11/08.
+//  Copyright © 2018 大城昂希. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var out: UILabel!
+    
+    @IBOutlet weak var SearchBar: UISearchBar!
+    
+    @IBAction func search(_ sender: Any) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
