16c73b8bec802abdc1c7dfb212be902ee6a9579b
kagari <e175766@ie.u-ryukyu.ac.jp>
2018-11-15 05:58:11 +0900

    preparated python script for slack api

--- Collection/Collection/ViewController.swift
+++ Collection/Collection/ViewController.swift
@@ -10,16 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var out: UILabel!
-    
-    @IBOutlet weak var SearchBar: UISearchBar!
-    
-    @IBAction func search(_ sender: Any) {
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBAction func button(_ sender: Any) {
+        if (searchBar.text == ""){
+            let alert = UIAlertController(title: "検索文字が入力されていません", message: "入力してください", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true)
+        }
     }
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Hello, World")
     }
-    
 }
