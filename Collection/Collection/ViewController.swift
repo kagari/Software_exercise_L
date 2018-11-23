ec8c9b0cf8730ec213a71b3cf30fd60f26c753af
kagari <e175766@ie.u-ryukyu.ac.jp>
2018-11-23 11:38:20 +0900

    検索時にapiを叩くように変更

--- Collection/Collection/ViewController.swift
+++ Collection/Collection/ViewController.swift
@@ -19,6 +19,33 @@ class ViewController: UIViewController {
             let alert = UIAlertController(title: "検索文字が入力されていません", message: "入力してください", preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
             self.present(alert, animated: true)
+        }else{
+            print(searchBar.text!)
+            // slackAPIを叩く
+            let url = URL(string: "https://slack.com/api/api.test")!
+            let task = URLSession.shared.dataTask(with: url) {
+                data, response, error in
+                
+                if let error = error {
+                    print("クライアントエラー: \(error.localizedDescription) \n")
+                    return
+                }
+                
+                guard let data = data, let response = response as? HTTPURLResponse else {
+                    print("no data or no response")
+                    return
+                }
+                
+                if response.statusCode == 200 {
+                    print("-------------------------")
+                    let str: String! = String(data: data, encoding: .utf8)
+                    print(str)
+                    print("-------------------------")
+                } else {
+                    print("サーバエラー ステータスコード: \(response.statusCode)\n")
+                }
+            }
+            task.resume()
         }
     }
     
@@ -35,6 +62,7 @@ class ViewController: UIViewController {
         
         // searchBarはその名の通り検索窓
         searchBar.placeholder = "キーワード検索" // デフォルトで表示される文字列を指定
+        
         // enterキーを押した時に画面遷移させるために書いた
         // TODO: enter keyを押した時にどうにか画面遷移させる
 //        if searchBar.isSearchResultsButtonSelected {
