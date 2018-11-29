2c5529154c29645dcee3bddccd174f8d92096e45
Taisei Arakaki <e175746@ie.u-ryukyu.ac.jp>
2018-11-29 10:32:44 +0900

    彩りの変更

--- Collection/Collection/NextViewController.swift
+++ Collection/Collection/NextViewController.swift
@@ -22,6 +22,23 @@ class NextViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //グラデーションをつける
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = self.view.bounds
+        
+        //グラデーションさせるカラーの設定
+        //今回は、徐々に色を濃くしていく
+        let color1 = UIColor(red: 120/256.0, green: 200/256.0, blue: 126/256.0, alpha: 0.5).cgColor
+        let color2 = UIColor(red: 120/256.0, green: 150/256.0, blue: 256/256.0, alpha: 0.5).cgColor
+        
+        //CAGradientLayerにグラデーションさせるカラーをセット
+        gradientLayer.colors = [color1, color2]
+        
+        //グラデーションの開始地点・終了地点の設定
+        //上が白で下が水色
+        gradientLayer.startPoint = CGPoint.init(x: 0.5, y: 0)
+        gradientLayer.endPoint = CGPoint.init(x: 0.5 , y:1 )
+        self.view.layer.insertSublayer(gradientLayer,at:0)
     }
     
 
