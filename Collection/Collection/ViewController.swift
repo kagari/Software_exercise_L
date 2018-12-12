780355b6ec1e58733a9fc6bc92a4d5e41d4ef4c9
Wataru Nakasone <e175729@ie.u-ryukyu.ac.jp>
2018-12-12 23:53:22 +0900

    なぜか回転しまくらないと描画できないグラデーション

--- Collection/Collection/ViewController.swift
+++ Collection/Collection/ViewController.swift
@@ -8,40 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    
-    @IBOutlet weak var label: UILabel!
+class ViewController: UIViewController  {
+    var tableView: UITableView!
+   @IBOutlet weak var label: UILabel!
     @IBOutlet weak var searchBar: UISearchBar!
-    @IBAction func button(_ sender: Any) {
+   @IBAction func button(_ sender: Any) {
         if (searchBar.text == ""){
             let alert = UIAlertController(title: "検索文字が入力されていません", message: "入力してください", preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
             self.present(alert, animated: true)
         }
+       
     }
     
-    override func viewDidLoad() {
+ /* override func viewDidLoad() {
         super.viewDidLoad()
-        print("Hello, World")
+        setView()}
+
+
+   // 回転を検知
+    override func viewDidAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(self.rotationChange(notification:)),
+                                               name: UIDevice.orientationDidChangeNotification, object: nil)
+        setView()
+    }
     
+    @objc
+    func rotationChange(notification: NSNotification){
+       // width = Int(view.frame.width)
+      //  height = Int(view.frame.height)
+    
+        setView()
+       
+    }
+    func setView() {
         
-        //グラデーションをつける
         let gradientLayer = CAGradientLayer()
-        gradientLayer.frame = self.view.bounds
+        gradientLayer.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
         
         //グラデーションさせるカラーの設定
         //今回は、徐々に色を濃くしていく
         let color1 = UIColor(red: 120/256.0, green: 170/256.0, blue: 126/256.0, alpha: 0.5).cgColor     //白
         let color2 = UIColor(red: 120/256.0, green: 150/256.0, blue: 256/256.0, alpha: 0.5).cgColor   //水色
-        
+        //gradientLayer.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
         //CAGradientLayerにグラデーションさせるカラーをセット
         gradientLayer.colors = [color1, color2]
         
         //グラデーションの開始地点・終了地点の設定
         //上が白で下が水色
-        gradientLayer.startPoint = CGPoint.init(x: 0.5, y: 0)
-        gradientLayer.endPoint = CGPoint.init(x: 0.5 , y:1 )
-        
+        gradientLayer.startPoint = CGPoint.init(x: 0, y:0.5 )
+        gradientLayer.endPoint = CGPoint.init(x: 1, y:0.5 )
+        //gradientLayer.frame = self.view.bounds
         //左が白で右が水色
         //gradientLayer.startPoint = CGPoint.init(x: 0, y: 0.5)
         //gradientLayer.endPoint = CGPoint.init(x: 1 , y:0.5)
@@ -52,5 +70,121 @@ class ViewController: UIViewController {
         
         //ViewControllerのViewレイヤーにグラデーションレイヤーを挿入する
         self.view.layer.insertSublayer(gradientLayer,at:0)
+        
+}
+    
+    
+}
+*/
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+        
+        //グラデーションさせるカラーの設定
+        //今回は、徐々に色を濃くしていく
+        let color1 = UIColor(red: 120/256.0, green: 170/256.0, blue: 126/256.0, alpha: 0.5).cgColor     //白
+        let color2 = UIColor(red: 120/256.0, green: 150/256.0, blue: 256/256.0, alpha: 0.5).cgColor   //水色
+        //gradientLayer.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        //CAGradientLayerにグラデーションさせるカラーをセット
+        gradientLayer.colors = [color1, color2]
+        
+        //グラデーションの開始地点・終了地点の設定
+        //上が白で下が水色
+        gradientLayer.startPoint = CGPoint.init(x: 0, y:0.5 )
+        gradientLayer.endPoint = CGPoint.init(x: 1, y:0.5 )
+        //gradientLayer.frame = self.view.bounds
+        //左が白で右が水色
+        //gradientLayer.startPoint = CGPoint.init(x: 0, y: 0.5)
+        //gradientLayer.endPoint = CGPoint.init(x: 1 , y:0.5)
+        
+        //左上が白で右下が水色
+        // gradientLayer.startPoint = CGPoint.init(x: 0, y: 0)
+        //gradientLayer.endPoint = CGPoint.init(x: 1 , y:1)
+        
+        //ViewControllerのViewレイヤーにグラデーションレイヤーを挿入する
+        self.view.layer.insertSublayer(gradientLayer,at:0)
+    
+    
+    }
+    override func viewDidAppear(_ animated: Bool) {
+       
+        // 端末の向きがかわったらNotificationを呼ばす設定.
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.onOrientationChange(notification:)), name:
+            UIDevice.orientationDidChangeNotification, object: nil)
+    } // 端末の向きがかわったら呼び出される.
+    @objc func onOrientationChange(notification: NSNotification){
+        let currentDevice: UIDevice = UIDevice.current
+        let orientation: UIDeviceOrientation = currentDevice.orientation //error here
+        // 現在のデバイスの向きを取得.
+        let _: UIDeviceOrientation!  = UIDevice.current.orientation
+       
+        // 向きの判定.
+        if orientation.isLandscape {
+            
+            //横向きの判定.
+            //向きに従って位置を調整する.
+            let gradientLayer = CAGradientLayer()
+            gradientLayer.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+            
+            //グラデーションさせるカラーの設定
+            //今回は、徐々に色を濃くしていく
+            let color1 = UIColor(red: 120/256.0, green: 170/256.0, blue: 126/256.0, alpha: 0.5).cgColor     //白
+            let color2 = UIColor(red: 120/256.0, green: 150/256.0, blue: 256/256.0, alpha: 0.5).cgColor   //水色
+            //gradientLayer.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+            //CAGradientLayerにグラデーションさせるカラーをセット
+            gradientLayer.colors = [color1, color2]
+            
+            //グラデーションの開始地点・終了地点の設定
+            //上が白で下が水色
+            gradientLayer.startPoint = CGPoint.init(x: 0, y:0.5 )
+            gradientLayer.endPoint = CGPoint.init(x: 1, y:0.5 )
+            //gradientLayer.frame = self.view.bounds
+            //左が白で右が水色
+            //gradientLayer.startPoint = CGPoint.init(x: 0, y: 0.5)
+            //gradientLayer.endPoint = CGPoint.init(x: 1 , y:0.5)
+            
+            //左上が白で右下が水色
+            // gradientLayer.startPoint = CGPoint.init(x: 0, y: 0)
+            //gradientLayer.endPoint = CGPoint.init(x: 1 , y:1)
+            
+            //ViewControllerのViewレイヤーにグラデーションレイヤーを挿入する
+            self.view.layer.insertSublayer(gradientLayer,at:0)
+            
+            
+        } else if orientation.isPortrait{
+            
+            //縦向きの判定.
+            //向きに従って位置を調整する.
+            let gradientLayer = CAGradientLayer()
+            gradientLayer.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+            
+            //グラデーションさせるカラーの設定
+            //今回は、徐々に色を濃くしていく
+            let color1 = UIColor(red: 120/256.0, green: 170/256.0, blue: 126/256.0, alpha: 0.5).cgColor     //白
+            let color2 = UIColor(red: 120/256.0, green: 150/256.0, blue: 256/256.0, alpha: 0.5).cgColor   //水色
+            //gradientLayer.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+            //CAGradientLayerにグラデーションさせるカラーをセット
+            gradientLayer.colors = [color1, color2]
+            
+            //グラデーションの開始地点・終了地点の設定
+            //上が白で下が水色
+            gradientLayer.startPoint = CGPoint.init(x: 0, y:0.5 )
+            gradientLayer.endPoint = CGPoint.init(x: 1, y:0.5 )
+            //gradientLayer.frame = self.view.bounds
+            //左が白で右が水色
+            //gradientLayer.startPoint = CGPoint.init(x: 0, y: 0.5)
+            //gradientLayer.endPoint = CGPoint.init(x: 1 , y:0.5)
+            
+            //左上が白で右下が水色
+            // gradientLayer.startPoint = CGPoint.init(x: 0, y: 0)
+            //gradientLayer.endPoint = CGPoint.init(x: 1 , y:1)
+            
+            //ViewControllerのViewレイヤーにグラデーションレイヤーを挿入する
+            self.view.layer.insertSublayer(gradientLayer,at:0)
+            
+            
+        }
+        
     }
 }
