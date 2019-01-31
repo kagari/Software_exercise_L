d7e1ddec592c1aaf8271afb482509771847cfc8e
Rito Mekaru <e175727@ie.u-ryukyu.ac.jp>
2019-01-31 15:51:18 +0900

    SlackとTwitterのボタンが両方押されてないときのアラート

--- Collection/Collection/ViewController.swift
+++ Collection/Collection/ViewController.swift
@@ -128,6 +128,10 @@ class ViewController: UIViewController, UISearchBarDelegate, UITableViewDelegate
             let alert = UIAlertController(title: "検索文字が入力されていません", message: "入力してください", preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
             self.present(alert, animated: true)
+        }else if Slack == false && Twitter == false{
+            let alert = UIAlertController(title: "検索結果がありませんでした", message: "検索したいアプリを選択してください", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true)
         }else{
             goToNextPage(message: searchBar.text!)
         }
